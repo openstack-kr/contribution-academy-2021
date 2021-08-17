fc79c280e4af4e864deabd2ac4688cd7c4329c06
choidoa-git <pearl097@naver.com>
2021-08-17 13:29:03 +0900

    Structure with RST based documentation env

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# setup.py
+
+import setuptools
+
+setuptools.setup(
+    name="tox-test",
+    author="Doa Choi",
+    author_email="pearl097@naver.com",
+    description="A package for test using tox",
+    packages=setuptools.find_packages(),
+)
